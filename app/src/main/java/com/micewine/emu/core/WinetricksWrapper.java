da7030cbb31f7ba49d5316383138f0c8cc63e81c
Windroid-emu <genrogamer@gmail.com>
2026-03-11 00:31:02 -0300

    fix(winetricks): use wrapper scripts for wine/wineserver execution

--- app/src/main/java/com/micewine/emu/core/WinetricksWrapper.java
+++ app/src/main/java/com/micewine/emu/core/WinetricksWrapper.java
@@ -1,6 +1,7 @@
 package com.micewine.emu.core;
 
 import static com.micewine.emu.activities.MainActivity.deviceArch;
+import static com.micewine.emu.activities.MainActivity.usrDir;
 import static com.micewine.emu.activities.MainActivity.winePrefix;
 import static com.micewine.emu.activities.MainActivity.winePrefixesDir;
 import static com.micewine.emu.core.EnvVars.getEnv;
@@ -14,13 +15,18 @@ public class WinetricksWrapper {
     }
 
     public static void winetricks(String args, String cwd) {
-        String wineCmd = IS_BOX64 + " wine";
-        String wineserverCmd = IS_BOX64 + " wineserver";
+        String wineWrapper = usrDir + "/bin/wine-wrapper";
+        String wineserverWrapper = usrDir + "/bin/wineserver-wrapper";
+
+        String setupWrappers = "echo '#!/bin/sh\\n" + IS_BOX64 + " wine \"$@\"' > " + wineWrapper + "; " +
+                "echo '#!/bin/sh\\n" + IS_BOX64 + " wineserver \"$@\"' > " + wineserverWrapper + "; " +
+                "chmod +x " + wineWrapper + " " + wineserverWrapper + "; ";
 
         runCommand(((cwd != null) ? "cd " + cwd + ";" : "") + getEnv() +
-                   "WINEPREFIX='" + winePrefixesDir + "/" + winePrefix + "' " +
-                   "WINE='" + wineCmd + "' " +
-                   "WINESERVER='" + wineserverCmd + "' " +
-                   "winetricks " + args, true);
+                setupWrappers +
+                "WINEPREFIX='" + winePrefixesDir + "/" + winePrefix + "' " +
+                "WINE='" + wineWrapper + "' " +
+                "WINESERVER='" + wineserverWrapper + "' " +
+                "winetricks " + args, true);
     }
 }
